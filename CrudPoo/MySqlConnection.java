bb2d8233246bf3c4bc82de36679423af8deee5c9
Cardenas Trujillo Alonso <hancel.cardenas.trujillo@vallegrande.edu.pe>
2024-10-31 18:57:24 -0500

    Add files via upload

---
+++ CrudPoo/MySqlConnection.java
@@ -0,0 +1,9 @@
+package com.mycompany.mysqlconnection;
+
+public class MySqlConnection {
+
+    public static void main(String[] args) {
+        Conexion c1 = new Conexion();
+        c1.establecerConexion();
+    }
+}
