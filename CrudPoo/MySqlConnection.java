a51aa4df602016864112c9d6080503aad48f3a53
Cardenas Trujillo Alonso <hancel.cardenas.trujillo@vallegrande.edu.pe>
2024-10-31 20:05:58 -0500

    Delete CrudPoo/MySqlConnection.java

--- CrudPoo/MySqlConnection.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.mysqlconnection;
-
-public class MySqlConnection {
-
-    public static void main(String[] args) {
-        Conexion c1 = new Conexion();
-        c1.establecerConexion();
-    }
-}
