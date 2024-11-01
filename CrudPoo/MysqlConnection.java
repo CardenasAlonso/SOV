794ab32d15d9f9d62b64c0bfb7d34d6c6ff27fc7
Cardenas Trujillo Alonso <hancel.cardenas.trujillo@vallegrande.edu.pe>
2024-10-31 20:06:46 -0500

    Add files via upload

---
+++ CrudPoo/MysqlConnection.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mysqlconnection;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class MysqlConnection {
+
+    public static void main(String[] args) {
+        Conexion c1 = new Conexion();
+        c1.getConexion();
+    }
+}
